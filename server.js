a43da7a4c2536f73589c61b0866ecbe98be88364
Vojtěch Bartoš <barvoj@seznam.cz>
2018-08-10 19:09:03 +0200

    Prepare heroku deploy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(8080, () => console.log('Server started on port 8080'));
+app.listen(port, () => console.log(`Server started on port ${port}`));
